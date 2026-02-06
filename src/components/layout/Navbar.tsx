d4f127e467c22070b85e13b129ff433533942196
[dyad] <38500332+krzysziomek@users.noreply.github.com>
2026-02-06 12:53:18 +0100

    Updated src/components/layout/Navbar.tsx

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,3 +1,2 @@
 <!DOCTYPE html>
-<meta http-equiv="refresh" content="0; url=https://modrinth.com/resourcepack/glowing-ores!">
 Jeśli nie zostaniesz przekierowany automatycznie, kliknij <a href="https://modrinth.com/resourcepack/glowing-ores!">tutaj</a>.
