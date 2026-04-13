0fefc47ce69b40486eb3007ad8180671829af06c
[dyad] <38500332+krzysziomek@users.noreply.github.com>
2026-04-13 13:12:03 +0200

    [dyad] Switching back to BrowserRouter - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Index } from "./pages/Index";
 import { Paczka } from "./pages/Paczka";
 import { Rozne } from "./pages/Rozne";
@@ -6,7 +6,7 @@ import { Layout } from "./components/layout/Layout";
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Layout>
         <Routes>
           <Route path="/" element={<Index />} />
@@ -14,7 +14,7 @@ function App() {
           <Route path="/rozne" element={<Rozne />} />
         </Routes>
       </Layout>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
